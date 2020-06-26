54c24d1128efe41afb8492ad251ab85241b6d4f9
Julian Dong <huydt@dgroup.co>
2020-06-26 21:26:44 +0700

    Updates

---
+++ precache-manifest.95f2368a1a09b9194fdc8be7264754ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3f762318a6328b7163c90ccf18b45c",
+    "url": "/personal-bmi/index.html"
+  },
+  {
+    "revision": "d9a3b83abd1da1343094",
+    "url": "/personal-bmi/static/css/main.38e7d22b.chunk.css"
+  },
+  {
+    "revision": "112dbae17cc7064e2c16",
+    "url": "/personal-bmi/static/js/2.8e29a79e.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/personal-bmi/static/js/2.8e29a79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a3b83abd1da1343094",
+    "url": "/personal-bmi/static/js/main.f7e396f9.chunk.js"
+  },
+  {
+    "revision": "236e8f3538f384d58939",
+    "url": "/personal-bmi/static/js/runtime-main.14dd7ce2.js"
+  }
+]);
